2091b49c865937646fe5993a64fb94296fa2a396
infosys <infosys@C4237>
2026-04-16 10:56:14 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter00/Sample0415/Sample0415/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Sample0415
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            //入力
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[" + i + "]=");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            //合計作業
+            //int sum = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    sum += array[i];
+            //}
+
+            //出力
+            for (int i = 0; i < array.Length; i++)
+            {
+                //Console.Write("array[" + i + "]:");
+
+                //アスタリスク出力
+                astOut(array[i]);
+            }
+
+            //合計出力            LINKって機能↓  これの場合は偶数のみを集計
+            Console.WriteLine("合計" + array.Where(n => n % 2 == 0).Sum());
+
+            static void astOut(int num)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write("*");
+                }
+                //改行用
+                Console.WriteLine();
+            }
+        }
+    }
+}
