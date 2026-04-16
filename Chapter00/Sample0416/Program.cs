53d41c7075565f3a849f3609aee24e9e97f66ae3
高橋　流星 <oibc37155@ojs.ac.jp>
2026-04-16 11:36:01 +0900

    10回出力するようにしました。

--- Chapter00/Sample0416/Program.cs
+++ Chapter00/Sample0416/Program.cs
@@ -2,9 +2,13 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello, World!");
+            }
+
         }
     }
 }
