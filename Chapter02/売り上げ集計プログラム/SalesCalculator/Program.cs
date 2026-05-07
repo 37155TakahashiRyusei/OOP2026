470ec377af89c5aa919769e08cd24c06339477bc
高橋　流星 <oibc37155@ojs.ac.jp>
2026-05-07 12:05:33 +0900

    p57まで

--- Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
@@ -1,10 +1,9 @@
 ﻿namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
-            //SalesCounter data = SalesCounter();
             var sales = new SalesCounter(@"data\sales.csv");
-            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string, int> obj in amountPerStore) {
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
             }
         }
