aa59065cf850f86337ef2e99287510914edd2f80
高橋　流星 <oibc37155@ojs.ac.jp>
2026-05-27 11:54:31 +0900

    Exercise3解説後のコード

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -47,7 +47,6 @@ namespace Exercise01 {
             }
         }
 
-
         private static void Exercise2(List<string> langs) {
             Console.WriteLine("\n ---4.1.2---");
             //LINQを使用する（where）
@@ -59,23 +58,23 @@ namespace Exercise01 {
 
         private static void Exercise3(List<string> langs) {
             Console.WriteLine("\n ---4.1.3---");
-            var lang = langs.Find(s => s.Length == 10);
+            var lang = langs.Find(s => s.Length == 10)?? "unknown";
+            Console.WriteLine(lang);
             //if () {
             //    Console.WriteLine(lang);
             //} else {
             //    Console.WriteLine("unknown");
             //}
 
-            var message = Known(langs) ?? Unknown();
-            Console.WriteLine(message);
+            //var message = Known(langs) ?? Unknown();
+            //Console.WriteLine(message);
         }
+        //private static object Unknown() {
+        //    return "null";
+        //}
 
-        private static object Unknown() {
-            return "null";
-        }
-
-        private static object? Known(object code) {
-            return code;
-        }
+        //private static object? Known(object code) {
+        //    return code;
+        //}
     }
 }
