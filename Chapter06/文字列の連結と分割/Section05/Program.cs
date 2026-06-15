85654b019c6d2c00ec1278dd8b9bdecb65c187e3
高橋　流星 <oibc37155@ojs.ac.jp>
2026-06-15 10:37:47 +0900

    カンマ区切りの解説

---
+++ Chapter06/文字列の連結と分割/Section05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+            var languages = Getwords();
+            //var sd = ", ";
+            var result = String.Join(",", Getwords());
+            Console.WriteLine(result);
+            //foreach (var word in Getwords()) {
+            //    Console.Write(word + sd);
+            //}
+        }
+
+        private static IEnumerable<string> Getwords() {
+            return ["Orange", "Lemon", "Strawberry"];
+        }
+    }
+}
