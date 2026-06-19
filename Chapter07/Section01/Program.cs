dd06c2aebeebbb2fa1cee0b411128a3e05906b54
高橋　流星 <oibc37155@ojs.ac.jp>
2026-06-19 10:17:42 +0900

    演習問題7_Exercise1完成

---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4 };
+
+            var books = Books.GetBooks();
+
+            var priceAverage = books.Average(x => x.Price);
+            var pageSum = books.Sum(x => x.Pages);
+            var maxPrice = books.Max(x => x.Price);
+
+            Console.WriteLine("平均金額:" + priceAverage);
+            Console.WriteLine("合計ページ:" + pageSum);
+            Console.WriteLine("高価な本:" + maxPrice);
+
+            Console.WriteLine();//改行用
+            Console.WriteLine("--- 500円以上のタイトル ---");
+            var money = books.Where(x => x.Price >= 500);
+            foreach (var item in money) {
+                Console.WriteLine(item.Title);
+            }
+
+            Console.WriteLine();//改行用
+            //本のページが250ページ以上の上位3冊を出力
+            Console.WriteLine("--- 本のページが250ページ以上の上位3冊 ---");
+
+            var top3 = books.Where(x => x.Pages >= 250).Take(3);
+            foreach (var top3Book in top3) {
+                Console.WriteLine(top3Book.Title);
+            }
+        }
+    }
+}
---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Collections.Generic;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+
+
+            Console.WriteLine("\n7.1.1");
+            Exercise1(numbers);
+
+            Console.WriteLine("\n7.1.2");
+            Exercise2(numbers);
+
+            Console.WriteLine("\n7.1.3");
+            Exercise3(numbers);
+
+            Console.WriteLine("\n7.1.4");
+            Exercise4(numbers);
+
+            Console.WriteLine("\n7.1.5");
+            Exercise5(numbers);
+        }
+
+        private static void Exercise1(int[] numbers) {
+            var max = numbers.Max();
+            Console.WriteLine(max);
+        }
+
+        private static void Exercise2(int[] numbers) {
+            var last = numbers.TakeLast(2);
+            foreach (var item in last) {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void Exercise3(int[] numbers) {
+            //P173参照
+           // var num = new List<int>
+        }
+
+        private static void Exercise4(int[] numbers) {
+            //P175参照
+            //var sort = numbers.OrderBy(x => x.)
+            //.ThenBy(x => x.);
+            //foreach (var item in sort) {
+            //    Console.WriteLine(item);
+            //}
+        }
+
+        private static void Exercise5(int[] numbers) {
+            //P174参照
+        }
+    }
+}
