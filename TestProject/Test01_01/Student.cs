c66192f7736e07cb02cb2a7b64ddfa05fa8fd769
高橋　流星 <oibc37155@ojs.ac.jp>
2026-06-22 11:27:10 +0900

    試験開始 設問１_Studentクラス完成

---
+++ TestProject/Test01_01/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace Test01_01 {
+    public class Student {
+
+        //学生の名前
+        public string Name { get; set; } = string.Empty;
+
+        //科目名
+        public string Subject { get; set; } = string.Empty;
+
+        //点数
+        public int Score { get; set; }
+
+    }
+}
