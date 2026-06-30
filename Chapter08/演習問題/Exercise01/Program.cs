955e6bde1ea9ce828fa7c7745fe98575a1297cf9
高橋　流星 <oibc37155@ojs.ac.jp>
2026-06-30 15:25:57 +0900

    問題8.1.1と8.1.2解説コード

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -13,23 +13,49 @@ namespace Exercise01 {
 
         private static void Exercise1(string text) {
             var dict = new Dictionary<char, int>();
-            var keydic = text.ToUpper().Replace(" ", "").OrderBy(x => x).ToArray();
-
-            var alphDicCount = Enumerable.Range('A', 26).
-                ToDictionary(num => ((char)num), num => 0);
-
-            foreach (var c in keydic) {
-                if (alphDicCount.ContainsKey(c))
-                    //登録されている場合
-                    alphDicCount[c]++;
+            foreach (var ch in text.ToUpper()) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch)) {
+                        dict[ch]++;
+                    } else {
+                        dict[ch] = 1;
+                    }
+                }
             }
-            foreach (var word in alphDicCount) {
-                Console.WriteLine(word.Key + ":" + word.Value);
+
+            foreach (var item in dict.OrderBy(x => x.Key)) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
             }
+            //var keydic = text.ToUpper().Replace(" ", "").OrderBy(x => x).ToArray();
+
+            //var alphDicCount = Enumerable.Range('A', 26).
+            //    ToDictionary(num => ((char)num), num => 0);
+
+            //foreach (var c in keydic) {
+            //    if (alphDicCount.ContainsKey(c))
+            //        //登録されている場合
+            //        alphDicCount[c]++;
+            //}
+            //foreach (var word in alphDicCount) {
+            //    Console.WriteLine(word.Key + ":" + word.Value);
+            //}
         }
 
         private static void Exercise2(string text) {
+            var dict = new SortedDictionary<char, int>();
+            foreach (var ch in text.ToUpper()) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch)) {
+                        dict[ch]++;
+                    } else {
+                        dict[ch] = 1;
+                    }
+                }
+            }
 
+            foreach (var item in dict) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
         }
     }
 }
