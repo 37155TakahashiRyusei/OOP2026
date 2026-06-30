d6a6717384d288f06389a83cad573385996466c3
高橋　流星 <oibc37155@ojs.ac.jp>
2026-06-30 14:48:01 +0900

    演習問題8.1_Exercise1完成

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections.Specialized;
+
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
@@ -10,8 +12,20 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(string text) {
+            var dict = new Dictionary<char, int>();
+            var keydic = text.ToUpper().Replace(" ", "").OrderBy(x => x).ToArray();
 
-           
+            var alphDicCount = Enumerable.Range('A', 26).
+                ToDictionary(num => ((char)num), num => 0);
+
+            foreach (var c in keydic) {
+                if (alphDicCount.ContainsKey(c))
+                    //登録されている場合
+                    alphDicCount[c]++;
+            }
+            foreach (var word in alphDicCount) {
+                Console.WriteLine(word.Key + ":" + word.Value);
+            }
         }
 
         private static void Exercise2(string text) {
