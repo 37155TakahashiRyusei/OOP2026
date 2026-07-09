a1ffd87f59cca50990611d43265277b929bc36ca
高橋　流星 <oibc37155@ojs.ac.jp>
2026-07-09 15:31:25 +0900

    演習問題9_Exercise01解説

--- Chapter09/演習問題/Exercise01/Form1.cs
+++ Chapter09/演習問題/Exercise01/Form1.cs
@@ -10,12 +10,12 @@ namespace Exercise01 {
         private void btButton1_Click(object sender, EventArgs e) {
             DateTime today = DateTime.Now;
             //tbOut1.Text = today.Date.ToString();
-            tbOut1.Text = today.ToString("yyyy/MM/dd HH:mm");
+            tbOut1.Text = string.Format($"{today}yyyy/MM/dd HH:mm");
         }
 
         private void btButton2_Click(object sender, EventArgs e) {
             DateTime today = DateTime.Now;
-            tbOut2.Text = today.ToString("yyyy”NMMŒŽdd“ú HHŽžmm•ªss•b");
+            tbOut2.Text = today.ToString($"{today}yyyy”NMMŒŽdd“ú HHŽžmm•ªss•b");
             // tbOut2.Text = $"{today.Year.ToString()}”N{today.Month}ŒŽ{today.Day}“ú{today.ToString()}";
         }
 
@@ -23,9 +23,15 @@ namespace Exercise01 {
             DateTime today = DateTime.Now;
             var culture = new CultureInfo("ja-JP");
             culture.DateTimeFormat.Calendar = new JapaneseCalendar();
-            var dayOfWeek = culture.DateTimeFormat.GetDayName(today.DayOfWeek);
 
-            tbOut3.Text = today.ToString( "gy”N" + " " + "MŒŽ" + " " + "d“ú" + "(" + dayOfWeek + ")", culture);
+            var cul = today.ToString("gg", culture); //˜a—ï
+            var dayOfweek = culture.DateTimeFormat.GetDayName(today.DayOfWeek);//—j“ú
+            var year = int.Parse(today.ToString("yy", culture));
+            var str2 = string.Format($"{cul}{year,2}”N{today.Month,2}ŒŽ{today.Day,2}“ú({dayOfweek})");
+            Console.WriteLine(str2);
+            //var dayOfWeek = culture.DateTimeFormat.GetDayName(today.DayOfWeek);
+
+            //tbOut3.Text = today.ToString( "gy”N" + " " + "MŒŽ" + " " + "d“ú" + "(" + dayOfWeek + ")", culture);
         }
     }
 }
