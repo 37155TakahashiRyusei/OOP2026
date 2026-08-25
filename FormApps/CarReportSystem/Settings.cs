e981c93960e643bc38309d207f575b58f2123d07
高橋　流星 <oibc37155@ojs.ac.jp>
2026-08-25 15:48:25 +0900

    SettingsクラスにSave処理を追加

--- FormApps/CarReportSystem/Settings.cs
+++ FormApps/CarReportSystem/Settings.cs
@@ -1,5 +1,10 @@
-﻿namespace CarReportSystem {
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+namespace CarReportSystem {
     public sealed class Settings {
+
+        private const string FileName = "setting.xml";
 
         //唯一のSettingオブジェクト
         private static readonly Settings _instance = new Settings();
@@ -14,8 +19,22 @@
         }
 
         //外部からnew出来ないようにする
-        private Settings() {
-            
+        private Settings() {}
+
+
+        public void Save() {
+            var data = new SetingsData {
+                MainFoemBackColor = MainFormBackColor
+            };
+
+            using var writer = XmlWriter.Create(FileName);
+            var serializer = new XmlSerializer(typeof(SetingsData));
+            serializer.Serialize(writer, data);
         }
+    }
+
+    //XML保存用のクラス
+    public class SetingsData {
+        public int MainFoemBackColor { get; set; }
     }
 }
