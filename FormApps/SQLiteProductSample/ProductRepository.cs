8910abee824c7f3d4ce1384674fbd4d2840a0b5a
高橋　流星 <oibc37155@ojs.ac.jp>
2026-09-01 15:49:58 +0900

    新規入力の作成途中

--- FormApps/SQLiteProductSample/ProductRepository.cs
+++ FormApps/SQLiteProductSample/ProductRepository.cs
@@ -1,11 +1,12 @@
 using Microsoft.Data.Sqlite;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace SQLiteProductSample;
 
 //Productテーブルに対するDB操作をまとめたクラス
 //CRUD　（Create / Read / Update / Delete  ）
-public class ProductRepository
-{
+public class ProductRepository {
     //全商品を取得する。Read(SELECT)に相当する
     public List<Product> GetAll() {
         var products = new List<Product>();
@@ -13,7 +14,7 @@ public class ProductRepository
         using var connection = Database.GetConnection();
         connection.Open();
 
-        //SQLを実行するためのｺﾏﾝﾄﾞオブジェクトうぃ作る
+        //SQLを実行するためのｺﾏﾝﾄﾞオブジェクトを作る
         using var command = connection.CreateCommand();
 
         //Productsテーブルを作るSQL
@@ -25,7 +26,7 @@ public class ProductRepository
             """;
 
         //SELECTを実行し、複数行の検索結果を読み取る
-        using var reader =  command.ExecuteReader();
+        using var reader = command.ExecuteReader();
 
         while (reader.Read()) {
             products.Add(new Product {
@@ -35,5 +36,82 @@ public class ProductRepository
             });
         }
         return products;
+    }
+
+    //商品を一件追加する。Create(INSERT)に相当する
+    //戻り値として自動採番されたIdを返す
+    public int Add(string name, int price) {
+        //接続オブジェクトを生成する
+        using var connection = Database.GetConnection();
+
+        //DBを開く
+        connection.Open();
+
+        //SQLを実行するためのｺﾏﾝﾄﾞオブジェクトを作る
+        using var command = connection.CreateCommand();
+
+        command.CommandText =
+            """
+            INSERT INTO Products (Name, Price)
+            VALUES ($name, $price);
+
+            SELECT last_insert_rowid();
+            """;
+
+        command.Parameters.AddWithValue("$name", name);
+        command.Parameters.AddWithValue("$price", price);
+
+        //一つの値を返すSQLを実行する
+        var result = command.ExecuteScalar();
+
+        if (result is null) {
+            throw new InvalidOperationException("登録した商品のIDを取得できませんでした。");
+        }
+
+        //SQLiteのINTEGERはlongとして返るため、intへ変換する
+        return Convert.ToInt32((long)result);
+    }
+
+    public void Update(Product product) {
+        //接続オブジェクトを生成する
+        using var connection = Database.GetConnection();
+
+        //DBを開く
+        connection.Open();
+
+        //SQLを実行するためのｺﾏﾝﾄﾞオブジェクトを作る
+        using var command = connection.CreateCommand();
+
+        command.CommandText =
+            """
+            UPDATE Products
+            SET Name = $name,
+                Price = $price
+            WHERE Id = $id;
+            """;
+
+        command.Parameters.AddWithValue("$name", product.Name);
+        command.Parameters.AddWithValue("$price", product.Price);
+        command.Parameters.AddWithValue($"id", product.Id);
+
+        //更新件数が0なら対象が存在しない
+        if (command.ExecuteNonQuery() == 0) {
+            throw new InvalidOperationException("修正対象の商品がみつかりませんでした。");
+        }
+    }
+
+    public void Delete (int id) {
+        using var connection = Database.GetConnection();
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText =
+            """
+            DELETE FROM Products
+            WHERE Id = $id;
+            """;
+
+        command.Parameters.AddWithValue("$id", id);
+        command.ExecuteNonQuery();
     }
 }
